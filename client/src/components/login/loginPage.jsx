92389d2ddcaa5ac231e3151afa0b0f84b01d83a1
Kartheek Kotha <kartheekkotha20041@gmail.com>
2024-04-13 02:04:17 +0530

    updated url

--- client/src/components/login/loginPage.jsx
+++ client/src/components/login/loginPage.jsx
@@ -12,7 +12,7 @@ const Login = ({ onLogin }) => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch(`${backendURL}/login`, {
+      const response = await fetch(`https://lms-react-server.vercel.app/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
